e0cbdb696041e5c5c078a33dbbc5a9077dc366ba
Wiktoria Kasprzak <kasprzak_w@yahoo.com>
2022-02-16 10:49:40 +0000

    added first css styling

--- src/components/ExpenseItem.js
+++ src/components/ExpenseItem.js
@@ -1,5 +1,16 @@
+import './ExpenseItem.css'
+
+
 function ExpenseItem() {
-    return <h2>Expense item!</h2>
+  return (
+    <div className="expense-item">
+      <idv>Date</idv>
+      <div className="expense-item__description">
+        <h2>Title</h2>
+        <div className="expense-item__price">Amount</div>
+      </div>
+    </div>
+  );
 }
 
 export default ExpenseItem;
