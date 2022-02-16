f9d5e7397eb849720971db5105b0e62f3ecc96d0
Wiktoria Kasprzak <kasprzak_w@yahoo.com>
2022-02-16 11:01:09 +0000

    changed from hard-coded code to dynamic

--- src/components/ExpenseItem.js
+++ src/components/ExpenseItem.js
@@ -2,12 +2,16 @@ import './ExpenseItem.css'
 
 
 function ExpenseItem() {
+    const expenseDate = new Date(2022, 2, 16);
+    const expenseTitle = 'Car Insurance';
+    const expenseAmount = 1200;
+
   return (
     <div className="expense-item">
-      <idv>Date</idv>
+      <idv>{expenseDate.toISOString()}</idv>
       <div className="expense-item__description">
-        <h2>Title</h2>
-        <div className="expense-item__price">Amount</div>
+        <h2>{expenseTitle}</h2>
+        <div className="expense-item__price">£{expenseAmount}</div>
       </div>
     </div>
   );
