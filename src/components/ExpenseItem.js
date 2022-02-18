9c000fba108b5fd5c2bf4b1042d5e4d0b38e3621
Wiktoria Kasprzak <kasprzak_w@yahoo.com>
2022-02-18 17:52:10 +0000

    fixed props bug

--- src/components/ExpenseItem.js
+++ src/components/ExpenseItem.js
@@ -3,11 +3,11 @@ import './ExpenseItem.css'
 
 function ExpenseItem(props) {
   return (
-    <div className="expense-item">
-      <idv>{props.Date.toISOString()}</idv>
-      <div className="expense-item__description">
+    <div className='expense-item'>
+      <idv>{props.date.toISOString()}</idv>
+      <div className='expense-item__description'>
         <h2>{props.title}</h2>
-        <div className="expense-item__price">£{props.Amount}</div>
+        <div className='expense-item__price'>£{props.amount}</div>
       </div>
     </div>
   );
