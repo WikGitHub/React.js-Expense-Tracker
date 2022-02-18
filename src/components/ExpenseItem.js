09b6d0359b65409673be8d101f14ca51c78f051e
Wiktoria Kasprzak <kasprzak_w@yahoo.com>
2022-02-18 18:08:22 +0000

    Added dynamic dates

--- src/components/ExpenseItem.js
+++ src/components/ExpenseItem.js
@@ -2,9 +2,17 @@ import './ExpenseItem.css'
 
 
 function ExpenseItem(props) {
-  return (
+    const month = props.date.toLocaleString('en-GB', {month: 'long'})
+    const day = props.date.toLocaleString('en-GB', {day: '2-digit'})
+    const year = props.date.getFullYear();
+
+    return (
     <div className='expense-item'>
-      <idv>{props.date.toISOString()}</idv>
+        <div>
+            <div>{day}</div>
+            <div>{month}</div>
+            <div>{year}</div>
+        </div>
       <div className='expense-item__description'>
         <h2>{props.title}</h2>
         <div className='expense-item__price'>£{props.amount}</div>
