08a8a5ecd6db16990f38a8843b738a2b90746323
Wiktoria Kasprzak <kasprzak_w@yahoo.com>
2022-02-18 17:45:49 +0000

    added first prop

--- src/components/ExpenseItem.js
+++ src/components/ExpenseItem.js
@@ -1,17 +1,13 @@
 import './ExpenseItem.css'
 
 
-function ExpenseItem() {
-    const expenseDate = new Date(2022, 2, 16);
-    const expenseTitle = 'Car Insurance';
-    const expenseAmount = 1200;
-
+function ExpenseItem(props) {
   return (
     <div className="expense-item">
-      <idv>{expenseDate.toISOString()}</idv>
+      <idv>{props.Date.toISOString()}</idv>
       <div className="expense-item__description">
-        <h2>{expenseTitle}</h2>
-        <div className="expense-item__price">£{expenseAmount}</div>
+        <h2>{props.title}</h2>
+        <div className="expense-item__price">£{props.Amount}</div>
       </div>
     </div>
   );
