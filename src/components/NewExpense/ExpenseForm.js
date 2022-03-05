95bf85f13b4acaa0d9ed0204c4e98302ff1b0428
Wiktoria Kasprzak <kasprzak_w@yahoo.com>
2022-03-05 21:11:14 +0000

    done 2 way binding

--- src/components/NewExpense/ExpenseForm.js
+++ src/components/NewExpense/ExpenseForm.js
@@ -1,4 +1,4 @@
-import Reac, {useState} from "react";
+import React, {useState} from "react";
 
 import './ExpenseForm.css';
 
@@ -29,6 +29,11 @@ const ExpenseForm = () => {
             amount: enteredAmount,
             date: new Date(enteredDate)
         };
+
+        console.log(expenseData)
+        setEnteredTitle('');
+        setEnteredAmount('');
+        setEnteredDate('');
     };
 
     return ( 
@@ -36,15 +41,15 @@ const ExpenseForm = () => {
             <div className = 'new-expense__controls'>
                 <div className = 'new-expense__control'>
                     <label>Title</label>
-                    <input type = 'text' onChange = {titleChangeHandler}/>
+                    <input type = 'text' value = {enteredTitle} onChange = {titleChangeHandler}/>
                 </div>
                 <div className = 'new-expense__control'>
                     <label>Amount</label>
-                    <input type = 'number' min = "0.01" step = "0.01" onChange = {amountChangeHandler}/>
+                    <input type = 'number' min = "0.01" step = "0.01" value={enteredAmount} onChange = {amountChangeHandler}/>
                 </div>
                 <div className = 'new-expense__control'>
                     <label>Date</label>
-                    <input type = 'date' min = "01-01-2022" max = "01-01-2050" onChange = {dateChangeHandler}/>
+                    <input type = 'date' min = "01-01-2022" max = "01-01-2050" value={enteredDate} onChange = {dateChangeHandler}/>
                 </div>
             </div>
             <div className = "new-expense__actions" >
