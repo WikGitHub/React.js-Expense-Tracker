14500233a6f134586a5feaeddd62ce8ac9d1ce7e
Wiktoria Kasprzak <kasprzak_w@yahoo.com>
2022-03-05 20:45:21 +0000

    used multiple statesd

--- src/components/NewExpense/ExpenseForm.js
+++ src/components/NewExpense/ExpenseForm.js
@@ -1,22 +1,38 @@
-import React from "react";
+import Reac, {useState} from "react";
 
 import './ExpenseForm.css';
 
 const ExpenseForm = () => {
+    const [enteredTitle, setEnteredTitle] = useState('');
+    const [enteredDate, setEnteredDate] = useState(''); // using string as the input value is always read as a string, even if its a number 
+    const [enteredAmount, setEnteredAmount] = useState('');
+
+    const titleChangeHandler = (event) => {
+        setEnteredTitle(event.target.value);
+    };
+
+    const dateChangeHandler = (event) => {
+        setEnteredDate(event.target.value);
+    };
+
+    const amountChangeHandler = (event) => {
+        setEnteredAmount(event.target.value);
+    };
+
     return ( 
         <form>
             <div className = 'new-expense__controls'>
                 <div className = 'new-expense__control'>
                     <label>Title</label>
-                    <input type = 'text'/>
+                    <input type = 'text' onChange = {titleChangeHandler}/>
                 </div>
                 <div className = 'new-expense__control'>
                     <label>Amount</label>
-                    <input type = 'number' min = "0.01" step = "0.01"/>
+                    <input type = 'number' min = "0.01" step = "0.01" onChange = {amountChangeHandler}/>
                 </div>
                 <div className = 'new-expense__control'>
                     <label>Date</label>
-                    <input type = 'date' min = "01-01-2022" max = "01-01-2050"/>
+                    <input type = 'date' min = "01-01-2022" max = "01-01-2050" onChange = {dateChangeHandler}/>
                 </div>
             </div>
             <div className = "new-expense__actions" >
